6dae6249932bcde78553a5fcddf1bea5715b6291
Bhavyai Gupta <zbhavyai@gmail.com>
2021-05-21 23:39:07 +0530

    Coded DaysTester for cmd line testing

--- src/DaysTester.java
+++ src/DaysTester.java
@@ -1,17 +1,18 @@
+import java.time.Month;
 
 /**
- * A class to test the Days class.
- * 
- * Runs through all the months 1 - 12 and prints the number of days.
- * 
- * @author 
+ * Class to test the working of Days class. Runs through all the months 1 - 12
+ * and prints the number of days in each month.
  *
+ * @author Bhavyai Gupta
+ * @version 1.0
+ * @since May 21, 2021
  */
 public class DaysTester {
-
 	public static void main(String[] args) {
-		
-
+		for (int i = 0; i < 12; i++) {
+			System.out.printf("Number of days in the month of %s is %d.%n", Month.of(i + 1).toString(),
+					Days.howManyDays(i + 1));
+		}
 	}
-
 }
